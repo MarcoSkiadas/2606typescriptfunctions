b1638a3e150f1c4b8cdbf0f3522a94d4d777a1ff
MarcoSkiadas <Skiadas2@hotmail.com>
2024-06-26 11:06:54 +0200

    Bonus task

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
 //Step 1: Create an array of numbers. Use the '.map' function to double all the numbers in the array.
-const numbers:number[] = [1,2,3,4,5,6]
+const numbers:number[] = [1,2,3,4,5,6];
 
 const doubleNumbers:number[] = numbers.map((num:number) => num*2);
 console.log("Array of Numbers: "+numbers+ "\nArray of doubleNumbers: "+doubleNumbers);
@@ -17,3 +17,15 @@ const numbers2:number[] = [1,2,3,4,5,6,11];
 const biggerThan10 = (element):boolean => element > 10;
 const numbers2WithNumberMoreThan10:boolean = numbers2.some(biggerThan10);
 console.log("Array of numbers2: "+numbers2+"\nIs there a number more than 10?: "+numbers2WithNumberMoreThan10);
+//Bonus
+const list:number[] = [15, 6, 3213, 9, 0, 12, 8464 , 1, 1264, 481, 186, 1031, 194];
+
+const result = list
+    .slice()                                                      // Make a copy of the list
+    .sort((a, b) => b - a)            // Sort in descending order.
+    .map(num => num * num)                              // Square these numbers.
+    .slice(4, -2)                                               // Remove the lowest two numbers and the highest 4 numbers from the list.
+    .filter(num => num % 4 !== 0)                      // Remove all numbers that are divisible by 4.
+    .reduce((acc, num) => acc + num, 0);     // Add up all the numbers.
+
+console.log(result);
